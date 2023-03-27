cc0de912db647d2021eaf3b75c38f779f44a455a
kanagatzhumagaliev <zhumagalievkanagat@gmail.com>
2023-03-27 14:17:55 +0600

    add datepicker plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import { createPinia } from 'pinia';
 import { router } from './router';
 import 'animate.css';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -16,4 +18,5 @@ const vuetify = createVuetify({
 import('@/style/main.scss')
 const app = createApp(App);
 const store = createPinia();
+app.component('date-picker', VueDatePicker);
 app.use(vuetify).use(store).use(router).mount('#app')
--- src/pages/community/communityDetailHistory.vue
+++ src/pages/community/communityDetailHistory.vue
@@ -23,6 +23,48 @@
         <v-select :items="['Светский', 'Агрессивный', 'Традиционный']" label="Состояние"/>
       </div>
     </div>
+    <div class="mt-5 mb-16 d-flex align-center">
+      <div class="date__picker mr-13">
+        <div class="picker__label">Дата начала</div>
+        <div class="date__block">
+          <span>{{ startDate }}</span>
+          <date-picker
+              v-model="sDate"
+              locale="ru"
+              :flow="flow"
+              model-type="dd.MM.yyyy"
+          >
+            <template #trigger>
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M6 1.33333C6.36819 1.33333 6.66667 1.63181 6.66667 2V2.66667H9.33333V2C9.33333 1.63181 9.63181 1.33333 10 1.33333C10.3682 1.33333 10.6667 1.63181 10.6667 2V2.66667H12.6667C13.403 2.66667 14 3.26362 14 4V12.6667C14 13.403 13.403 14 12.6667 14H3.33333C2.59695 14 2 13.403 2 12.6667V4C2 3.26362 2.59695 2.66667 3.33333 2.66667H5.33333V2C5.33333 1.63181 5.63181 1.33333 6 1.33333ZM5.33333 4H3.33333V6H12.6667V4H10.6667V4.66667C10.6667 5.03486 10.3682 5.33333 10 5.33333C9.63181 5.33333 9.33333 5.03486 9.33333 4.66667V4H6.66667V4.66667C6.66667 5.03486 6.36819 5.33333 6 5.33333C5.63181 5.33333 5.33333 5.03486 5.33333 4.66667V4ZM12.6667 7.33333H3.33333V12.6667H12.6667V7.33333Z"
+                    fill="#4685D7"/>
+              </svg>
+            </template>
+          </date-picker>
+        </div>
+      </div>
+      <div class="date__picker">
+        <div class="picker__label">Дата окончания</div>
+        <div class="date__block">
+          <span>{{ endDate }}</span>
+          <date-picker
+              v-model="eDate"
+              locale="ru"
+              :flow="flow"
+              model-type="dd.MM.yyyy"
+          >
+            <template #trigger>
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M6 1.33333C6.36819 1.33333 6.66667 1.63181 6.66667 2V2.66667H9.33333V2C9.33333 1.63181 9.63181 1.33333 10 1.33333C10.3682 1.33333 10.6667 1.63181 10.6667 2V2.66667H12.6667C13.403 2.66667 14 3.26362 14 4V12.6667C14 13.403 13.403 14 12.6667 14H3.33333C2.59695 14 2 13.403 2 12.6667V4C2 3.26362 2.59695 2.66667 3.33333 2.66667H5.33333V2C5.33333 1.63181 5.63181 1.33333 6 1.33333ZM5.33333 4H3.33333V6H12.6667V4H10.6667V4.66667C10.6667 5.03486 10.3682 5.33333 10 5.33333C9.63181 5.33333 9.33333 5.03486 9.33333 4.66667V4H6.66667V4.66667C6.66667 5.03486 6.36819 5.33333 6 5.33333C5.63181 5.33333 5.33333 5.03486 5.33333 4.66667V4ZM12.6667 7.33333H3.33333V12.6667H12.6667V7.33333Z"
+                    fill="#4685D7"/>
+              </svg>
+            </template>
+          </date-picker>
+        </div>
+      </div>
+    </div>
     <div class="content__body">
       <div class="content__container">
         <div class="d-flex mt-6 pt-6 align-center justify-space-between">
@@ -105,7 +147,15 @@
 </template>
 <script setup>
 import Icon from "@/components/icon.vue";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
-const ex1 = ref()
+const sDate = ref()
+const eDate = ref()
+const flow = ['calendar']
+const startDate = computed(() => {
+  return sDate.value || 'ДД.ММ.ГГГГ'
+})
+const endDate = computed(() => {
+  return eDate.value || 'ДД.ММ.ГГГГ'
+})
 </script>
